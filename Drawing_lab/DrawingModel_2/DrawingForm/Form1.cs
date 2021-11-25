9cf745d5bf4eb2d3df39464443feb1fb3278c45c
Leaf Ying <leaf_ying@gss.com.tw>
2021-11-25 17:44:35 +0800

    Add drawing lab projects

---
+++ Drawing_lab/DrawingModel_2/DrawingForm/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawingForm
+{
+    public partial class Form1 : Form
+    {
+        private DrawingModel.Model _model;
+        private PresentationModel.PresentationModel _presentationModel;
+        private readonly Panel _canvas;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _canvas = new DoubleBufferedPanel();
+
+            // prepare canvas
+            _canvas.Dock = DockStyle.Fill;
+            _canvas.BackColor = System.Drawing.Color.LightYellow;
+            _canvas.MouseDown += HandleCanvasPressed;
+            _canvas.MouseUp += HandleCanvasReleased;
+            _canvas.MouseMove += HandleCanvasMoved;
+            _canvas.Paint += HandleCanvasPaint;
+            Controls.Add(_canvas);
+
+            // prepare clear button
+            Button clear = new Button();
+            clear.Text = "Clear";
+            clear.Dock = DockStyle.Top;
+            clear.AutoSize = true;
+            clear.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            clear.Click += HandleClearButtonClick;
+            Controls.Add(clear);
+
+            // prepare presentation model and model
+            _model = new DrawingModel.Model();
+            _presentationModel = new PresentationModel.PresentationModel(_model,
+           _canvas);
+            _model.ModelChanged += HandleModelChanged;
+        }
+
+        public void HandleClearButtonClick(object sender, System.EventArgs e)
+        {
+            _model.Clear();
+        }
+
+        public void HandleCanvasPressed(object sender, MouseEventArgs e)
+        {
+            _model.PointerPressed(e.X, e.Y);
+        }
+
+        public void HandleCanvasReleased(object sender, MouseEventArgs e)
+        {
+            _model.PointerReleased(e.X, e.Y);
+        }
+
+        public void HandleCanvasMoved(object sender, MouseEventArgs e)
+        {
+            _model.PointerMoved(e.X, e.Y);
+        }
+
+        public void HandleCanvasPaint(object sender, PaintEventArgs e)
+        {
+            _presentationModel.Draw(e.Graphics);
+        }
+
+        public void HandleModelChanged()
+        {
+            Invalidate(true);
+        }
+    }
+}
---
+++ Drawing_lab/DrawingModel_2/DrawingModel/Model.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+
+namespace DrawingModel
+{
+    public class Model
+    {
+        public event ModelChangedEventHandler ModelChanged;
+        public delegate void ModelChangedEventHandler();
+
+        private double _firstPointX;
+        private double _firstPointY;
+        private bool _isPressed;
+        private readonly List<Line> _lines;
+        private readonly Line _hint;
+
+        public Model()
+        {
+            _firstPointX = _firstPointY = -1;
+            _isPressed = false;
+            _lines = new List<Line>();
+            _hint = new Line();
+        }
+
+        public void PointerPressed(double x, double y)
+        {
+            if (x > 0 && y > 0)
+            {
+                _firstPointX = x;
+                _firstPointY = y;
+                _hint.x1 = _firstPointX;
+                _hint.y1 = _firstPointY;
+                _isPressed = true;
+            }
+        }
+
+        public void PointerMoved(double x, double y)
+        {
+            if (_isPressed)
+            {
+                _hint.x2 = x;
+                _hint.y2 = y;
+                NotifyModelChanged();
+            }
+        }
+
+        public void PointerReleased(double x, double y)
+        {
+            if (_isPressed)
+            {
+                _isPressed = false;
+
+                var hint = new Line();
+                hint.x1 = _firstPointX;
+                hint.y1 = _firstPointY;
+                hint.x2 = x;
+                hint.y2 = y;
+                _lines.Add(hint);
+
+                NotifyModelChanged();
+            }
+        }
+
+        public void Clear()
+        {
+            _isPressed = false;
+            _lines.Clear();
+
+            NotifyModelChanged();
+        }
+
+        public void Draw(IGraphics graphics)
+        {
+            graphics.ClearAll();
+            foreach (var line in _lines)
+            {
+                line.Draw(graphics);
+            }
+            if (_isPressed)
+            {
+                _hint.Draw(graphics);
+            }
+        }
+
+        void NotifyModelChanged()
+        {
+            if (ModelChanged != null)
+                ModelChanged();
+        }
+    }
+}
